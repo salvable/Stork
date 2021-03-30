f1b039e1a53de404959df2b762b190cec0ba0ac9
salvable <salvable@kakao.com>
2021-03-30 19:16:55 +0900

    addmoney 작성중

--- controller/account.controller.js
+++ controller/account.controller.js
@@ -1,8 +1,15 @@
+const createError = require('http-errors')
+
 exports.addMoney = async (req, res, next) => {
     const accountId = req.params.accountId
-    const money = req.query
+    const money = req.query.money
+
+    if(!accountId || !money){
+        return next(createError(400, 'BadRequestError'))
+    }
 
     try {
+
 
     } catch (err) {
         return res.status(500).json(err)
