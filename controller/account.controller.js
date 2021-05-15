4368d9296bd1b8b48efd0b6f6122ed16ef5d873a
salvable <salvable@kakao.com>
2021-05-15 17:58:00 +0900

    getAccount 추가

--- controller/account.controller.js
+++ controller/account.controller.js
@@ -1,7 +1,31 @@
 const createError = require('http-errors')
 const accountService = require('../service/account.service')
 
-exports.addMoney = async (req, res, next) => {
+exports.getAccount = async (req, res, next) => {
+    const userId = req.params.userId
+
+    if(!userId){
+        return next(createError(400, 'BadRequestError'))
+    }
+
+    try {
+        const account = await accountService.getAccount(userId)
+
+        if(account == "NotFoundError"){
+            return next(createError(404, 'NotFoundError'))
+        }
+
+        return res.send(
+            {
+                account: account
+            }
+        )
+    } catch (err) {
+        return res.status(500).json(err)
+    }
+}
+
+exports.updateMoney = async (req, res, next) => {
     const accountId = req.params.accountId
     const userId = req.query.userId
     const money = req.query.money
