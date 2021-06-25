8562d1ce1fe4809ca1de20c8f3290e827fd13651
salvable <salvable@kakao.com>
2021-06-25 15:40:41 +0900

    user model 수정

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ module.exports = (sequelize, DataTypes) => {
     const user =  sequelize.define('user', {
         userId: {type: DataTypes.STRING(20), allowNull: false, primaryKey: true},
         password: { type: DataTypes.STRING(100), allowNull: false, },
-        email: {type: DataTypes.STRING(20), allowNull: false, unique: true,},
+        email: {type: DataTypes.STRING(20), allowNull: false,},
         name: { type: DataTypes.STRING(10), allowNull: false, },
         phoneNumber: {type: DataTypes.STRING(20), allowNull: false}
     }, { timestamps:false, });
