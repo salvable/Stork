d4262ed518834c9a740529d4505845f14a1851ef
salvable <salvable@kakao.com>
2021-07-17 21:56:36 +0900

    account subMoney추가

--- controller/account.controller.js
+++ controller/account.controller.js
@@ -64,3 +64,43 @@ exports.addMoney = async (req, res, next) => {
         }
     }
 }
+
+exports.subMoney = async (req, res, next) => {
+    const accountId = req.params.accountId
+    const userId = req.query.userId
+    const money = req.query.money
+
+
+    if(!accountId || !money){
+        const err = new Error('BadRequestError')
+        err.name = 'BadRequestError'
+        throw err
+    }
+
+    try {
+        const account =await accountService.subMoney(accountId,money)
+
+        if(account == 'NotFoundError'){
+            const err = new Error(account)
+            err.name = account
+            throw err
+        }
+
+        const updateAccount = await accountService.getAccount(userId)
+
+        return res.send(
+            {
+                account: updateAccount
+            }
+        )
+    } catch (err) {
+        switch(err.name){
+            case "Bad request":
+                return next(createError(400, 'Bad request'))
+            case "NotFoundError":
+                return next(createError(404, 'NotFoundError'))
+            default:
+                return next(createError(500, 'Error'))
+        }
+    }
+}
