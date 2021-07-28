931f713ea9214b9621d81859a027dbdb8c636d45
salvable <salvable@kakao.com>
2021-07-28 20:30:13 +0900

    기존작업 롤백

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-const db = require('./models')
-const User = db["user"]
-
 const userController = require('./controller/user.controller')
 const authController = require('./controller/auth.controller')
 const accountController = require('./controller/account.controller')
@@ -8,75 +5,23 @@ const storkController = require('./controller/stork.controller')
 const favoriteController = require('./controller/favorite.controller')
 const authMiddleware = require('./middleware/auth.middleware')
 
-
 const passport = require('passport');
 const passportConfig = require('./passport');
-const cookieParser = require('cookie-parser');
 const cors = require('cors')
 const express = require('express')
 const bodyParser = require('body-parser')
-const session = require("express-session");
 const sequelize = require('./models').sequelize;
 const app = express()
 const port = 3000;
 
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));  // 클라이언트의 form값을 req.body에 넣음
-app.use(cors({
-        origin: [
-            "http://localhost:3001",
-        ],
-        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-        preflightContinue: false,
-        optionsSuccessStatus: 204,
-        credentials: true,
-    })
-);
-app.use(require('express-session')({
-    secret: 'keyboard cat',
-    resave: true,
-    saveUninitialized: true,
-    cookie:{
-        maxAge:30000,
-        secure:false,
-        httpOnly:false
-    }
-}));
+app.use(cors());
 app.use(passport.initialize());
-app.use(passport.session());
 passportConfig();
 app.use(function(err, req, res, next) {
     res.status(500).send('Something broke!');
 });
-
-passport.serializeUser((user, done) => {
-    console.log('serializeUser() 호출됨.');
-    done(null, user.userId);
-    //session에 user.userId 저장
-});
-//사용자가 페이지를 조회할 때마다 식별자로 사용자 확인
-
-passport.deserializeUser((userId, done) => { // 매개변수 id는 req.session.passport.user에 저장된 값
-    User.findById(userId, (err, user) => {
-        console.log("#####")
-        done(null, user); // 여기의 user가 req.user가 됨
-
-    });
-});
-
-// passport.deserializeUser((userId, done) =>{
-//     try{
-//         console.log(userId)
-//         const user = User.findOne({ where: { userId: userId }})
-//         console.log("#####")
-//         console.log(user)
-//         done((null, user))
-//     }catch (e){
-//         done(e)
-//     }
-// })
-
-
 
 app.get ( '/', (req, res) => {res.send ( 'Hello Api Server!!!' +
     '');});
