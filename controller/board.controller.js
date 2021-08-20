63075bc207d3c5f608e93151652ccd48d3240cd1
salvable <salvable@kakao.com>
2021-08-20 18:08:43 +0900

    addBoard body 받는 부분 수정

--- controller/board.controller.js
+++ controller/board.controller.js
@@ -4,9 +4,9 @@ const boardService =require("../service/board.service")
 
 exports.addBoard = async (req, res, next) => {
     const name = req.body.userId
-    const content = req.body.password
-    const writer = req.body.email
-    const password = req.body.phoneNumber
+    const content = req.body.content
+    const writer = req.body.writer
+    const password = req.body.password
 
     if(!name || !content || !writer || !password){
         return next(createError(400, 'Bad request'))
