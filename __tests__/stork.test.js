074fc08d59e17ba6bb08499be2ac0c318b0cd37a
salvable <salvable@kakao.com>
2022-04-21 20:26:13 +0900

    stork testcase 작성

--- __tests__/stork.test.js
+++ __tests__/stork.test.js
@@ -34,23 +34,117 @@ it('POST /Login 200', async () => {
 
 // 임시로 storkName, number, price를 설정, 원래는 Django서버로부터 크롤링한 값을 가져옴
 it('POST /stork/purchase/:userId/:accountId 200', async () => {
-    const response = await request(app).post(`/stork/purchase/${userId}/${accountId}?storkName="${encodeURI("삼성전자")}"&number=5&price=70000`).set(
+    const response = await request(app).post(`/stork/purchase/${userId}/${accountId}?storkName=${encodeURI("삼성전자")}&number=14&price=70000`).set(
         'Authorization' ,  `Bearer ${accessToken}`
-    );;
+    );
 
     expect(response.statusCode).toBe(200)
 });
 
-it('POST /stork/purchase/:userId/:accountId 404', async () => {
-    const response = await request(app).post(`/stork/purchase/${userId}/${accountId}?storkName="${encodeURI("삼성전자")}"&number=500&price=70000`).set(
+it('POST /stork/purchase/:userId/:accountId 400', async () => {
+    const response = await request(app).post(`/stork/purchase/${userId}/${accountId}?storkName=${encodeURI("삼성전자")}&number=500&price=70000`).set(
         'Authorization' ,  `Bearer ${accessToken}`
-    );;
-
+    );
 
     expect(response.statusCode).toBe(400)
 });
 
 
+it('POST /stork/purchase/:userId/:accountId 400', async () => {
+    const response = await request(app).post(`/stork/purchase/${userId}/${accountId}?storkName=${encodeURI("삼성전자")}`).set(
+        'Authorization' ,  `Bearer ${accessToken}`
+    );
+
+    expect(response.statusCode).toBe(400)
+});
+
+it('POST /stork/purchase/:userId/:accountId 2 404', async () => {
+    const response = await request(app).post(`/stork/purchase/${userId}/NotFoundTest?storkName=${encodeURI("삼성전자")}&number=5&price=70000`).set(
+        'Authorization' ,  `Bearer ${accessToken}`
+    );
+
+    expect(response.statusCode).toBe(404)
+});
+
+it('POST /stork/sale/:userId/:accountId 200', async () => {
+    const response = await request(app).post(`/stork/sale/${userId}/${accountId}?storkName=${encodeURI("삼성전자")}&number=10&price=70000`).set(
+        'Authorization' ,  `Bearer ${accessToken}`
+    );
+
+    expect(response.statusCode).toBe(200)
+});
+
+it('POST /stork/sale/:userId/:accountId 400', async () => {
+    const response = await request(app).post(`/stork/sale/${userId}/${accountId}?storkName=${encodeURI("삼성전자")}`).set(
+        'Authorization' ,  `Bearer ${accessToken}`
+    );
+
+    expect(response.statusCode).toBe(400)
+});
+
+it('POST /stork/sale/:userId/:accountId 2 400', async () => {
+    const response = await request(app).post(`/stork/sale/${userId}/${accountId}?storkName=${encodeURI("삼성전자")}&number=500&price=70000`).set(
+        'Authorization' ,  `Bearer ${accessToken}`
+    );
+
+    expect(response.statusCode).toBe(400)
+});
+
+it('POST /stork/sale/:userId/:accountId 404', async () => {
+    const response = await request(app).post(`/stork/sale/${userId}/Account?storkName=${encodeURI("삼성전자")}&number=5&price=70000`).set(
+        'Authorization' ,  `Bearer ${accessToken}`
+    );
+
+    expect(response.statusCode).toBe(404)
+});
+
+it('POST /stork/sale/:userId/:accountId 2 404', async () => {
+    const response = await request(app).post(`/stork/sale/${userId}/${accountId}?storkName=NoStork&number=5&price=70000`).set(
+        'Authorization' ,  `Bearer ${accessToken}`
+    );
+
+    expect(response.statusCode).toBe(404)
+});
+
+it('GET /stork/:storkName/:userId 200', async () => {
+    const response = await request(app).get(`/stork/${encodeURI("삼성전자")}/${userId}`).set(
+        'Authorization' ,  `Bearer ${accessToken}`
+    );
+
+    expect(response.statusCode).toBe(200)
+});
+
+it('GET /stork/:storkName/:userId 404', async () => {
+    const response = await request(app).get(`/stork/${encodeURI("샘송전자")}/${userId}`).set(
+        'Authorization' ,  `Bearer ${accessToken}`
+    );
+
+    expect(response.statusCode).toBe(404)
+});
+
+it('GET /storks/:userId 200', async () => {
+    const response = await request(app).get(`/storks/${userId}`).set(
+        'Authorization' ,  `Bearer ${accessToken}`
+    );
+
+    expect(response.statusCode).toBe(200)
+});
+
+it('GET /storks/:userId 200', async () => {
+    const response = await request(app).get(`/storks/${userId}`).set(
+        'Authorization' ,  `Bearer ${accessToken}`
+    );
+
+    expect(response.statusCode).toBe(200)
+});
+
+it('GET /storks/:userId 404', async () => {
+    const response = await request(app).get(`/storks/NotFoundCheck`).set(
+        'Authorization' ,  `Bearer ${accessToken}`
+    );
+
+    expect(response.statusCode).toBe(404)
+});
 
 it('DELETE /user/:userId 200' , async () => {
     const response = await request(app).delete(`/user/${userId}?password=${password}`)
