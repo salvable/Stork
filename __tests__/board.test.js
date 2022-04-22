18582a3841788b5840a8405e7bd78650536028f6
salvable <salvable@kakao.com>
2022-04-22 20:12:53 +0900

    board TestCase작

--- __tests__/board.test.js
+++ __tests__/board.test.js
@@ -3,8 +3,9 @@ const app = require("../app")
 
 const userId = "testJest"
 const password = "test1234"
+const boardPassword = "1q2w3e4r!"
+let boardId = ""
 let accessToken = ""
-let accountId = ""
 
 it('POST /user 200', async () => {
     const response = await request(app).post('/user').send({
@@ -15,8 +16,6 @@ it('POST /user 200', async () => {
         name : "JEST",
         phoneNumber : "010-4548-8451"
     });
-
-    accountId = JSON.parse(response.res.text).account.accountId
 
     expect(response.statusCode).toBe(200)
 });
@@ -32,6 +31,152 @@ it('POST /Login 200', async () => {
     expect(response.statusCode).toBe(200)
 });
 
+it('POST /board 200', async () => {
+    const response = await request(app).post('/board').send({
+       name: "게시판 테스트",
+       content: "게시판 테스트 입니다~",
+       writer: userId,
+       password: boardPassword
+    }).set(
+        'Authorization' ,  `Bearer ${accessToken}`
+    );
+
+    boardId = JSON.parse(response.res.text).board.boardId
+
+    expect(response.statusCode).toBe(200)
+});
+
+it('POST /board 400', async () => {
+    const response = await request(app).post('/board').send({
+        writer: userId,
+        password: boardPassword
+    }).set(
+        'Authorization' ,  `Bearer ${accessToken}`
+    );
+
+    expect(response.statusCode).toBe(400)
+});
+
+it('PUT /board/:boardId 200', async () => {
+    const response = await request(app).put(`/board/${boardId}`).send({
+        name: "게시판 업데이트 테스트!!!!!",
+        content: "게시판 업데이트를 테스트해봅니다.",
+        password: boardPassword
+    });
+
+    expect(response.statusCode).toBe(200)
+});
+
+it('PUT /board/:boardId 400', async () => {
+    const response = await request(app).put(`/board/${boardId}`).send({
+        password: boardPassword
+    });
+
+    expect(response.statusCode).toBe(400)
+});
+
+it('PUT /board/:boardId 403', async () => {
+    const response = await request(app).put(`/board/${boardId}`).send({
+        name: "게시판 업데이트 테스트!!!!!",
+        content: "게시판 업데이트를 테스트해봅니다.",
+        password: "Fobbiden Test"
+    });
+
+    expect(response.statusCode).toBe(403)
+});
+
+it('GET /boards 200', async () => {
+    const response = await request(app).get(`/boards?page=1`)
+
+    expect(response.statusCode).toBe(200)
+});
+
+it('GET /boards 400', async () => {
+    const response = await request(app).get(`/boards?page=0`)
+
+    expect(response.statusCode).toBe(400)
+});
+
+it('GET /boards 2 400', async () => {
+    const response = await request(app).get(`/boards`)
+
+    expect(response.statusCode).toBe(400)
+});
+
+it('GET /board/:boardId 200', async () => {
+    const response = await request(app).get(`/board/${boardId}`)
+
+    expect(response.statusCode).toBe(200)
+});
+
+it('GET /board/:boardId 404', async () => {
+    const response = await request(app).get(`/board/999999`)
+
+    expect(response.statusCode).toBe(404)
+});
+
+it('PUT /board/:boardId/star 200', async () => {
+    const response = await request(app).put(`/board/${boardId}/star`).send({
+        userId: userId,
+        type: "star"
+    }).set(
+        'Authorization' ,  `Bearer ${accessToken}`
+    );
+
+    expect(response.statusCode).toBe(200)
+});
+
+it('PUT /board/:boardId/star 404', async () => {
+    const response = await request(app).put(`/board/999999/star`).send({
+        userId: userId,
+        type: "unstar"
+    }).set(
+        'Authorization' ,  `Bearer ${accessToken}`
+    );
+
+    expect(response.statusCode).toBe(404)
+});
+
+it('PUT /board/:boardId/star 409', async () => {
+    const response = await request(app).put(`/board/${boardId}/star`).send({
+        userId: userId,
+        type: "unstar"
+    }).set(
+        'Authorization' ,  `Bearer ${accessToken}`
+    );
+
+    expect(response.statusCode).toBe(409)
+});
+
+it('GET /board/:boardId/checkPw 200', async () => {
+    const response = await request(app).get(`/board/${boardId}/checkPw?password=${boardPassword}`)
+
+    expect(response.statusCode).toBe(200)
+});
+
+it('GET /board/:boardId/checkPw 400', async () => {
+    const response = await request(app).get(`/board/${boardId}/checkPw?password=passwordTest`)
+
+    expect(response.statusCode).toBe(400)
+});
+
+it('DELETE /board/:boardId 400' , async () => {
+    const response = await request(app).delete(`/board/${boardId}?`)
+
+    expect(response.statusCode).toBe(400)
+});
+
+it('DELETE /board/:boardId 2 400' , async () => {
+    const response = await request(app).delete(`/board/${boardId}?password=passwordTest`)
+
+    expect(response.statusCode).toBe(400)
+});
+
+it('DELETE /board/:boardId' , async () => {
+    const response = await request(app).delete(`/board/${boardId}?password=${boardPassword}`)
+
+    expect(response.statusCode).toBe(200)
+});
 
 it('DELETE /user/:userId 200' , async () => {
     const response = await request(app).delete(`/user/${userId}?password=${password}`)
